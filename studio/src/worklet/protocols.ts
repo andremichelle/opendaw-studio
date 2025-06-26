22c075d1630714376761232513ae92e4f26239f3
André Michelle <andre.michelle@gmail.com>
2025-06-26 09:11:48 +0200

    adds comments

--- studio/src/worklet/protocols.ts
+++ studio/src/worklet/protocols.ts
@@ -8,11 +8,15 @@ export interface EngineCommands extends Terminable {
     setRecording(value: boolean): void
     setPosition(position: ppqn): void
     setMetronomeEnabled(enabled: boolean): void
+    // full stop and clear all buffers
     stopAndReset(): void
     queryLoadingComplete(): Promise<boolean>
+    // throws a test error while processing audio
     panic(): void
+    // feeds a note request into an audio-unit identified by uuid
     noteOn(uuid: UUID.Format, pitch: byte, velocity: unitValue): void
     noteOff(uuid: UUID.Format, pitch: byte): void
+    // timeline clip playback management
     scheduleClipPlay(clipIds: ReadonlyArray<UUID.Format>): void
     scheduleClipStop(trackIds: ReadonlyArray<UUID.Format>): void
 }
