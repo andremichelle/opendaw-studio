e8426ac2f9fa703b961dd4dafec7d359ee076191
André Michelle <andre.michelle@gmail.com>
2025-06-27 07:23:57 +0200

    adds install to root

--- rebuild.sh
+++ rebuild.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 export TERM=${TERM:-dumb}
 if [[ -t 1 ]]; then clear; fi
-bash ./clean.sh           || exit 1
+bash ./clean.sh || exit 1
 echo "rebuild lib"
 (cd lib && bash ./rebuild.sh) || exit 1
 echo "rebuild core"
 (cd core && bash ./rebuild.sh) || exit 1
 echo "install studio"
-(cd studio && npm install)         || exit 1
+(cd studio && npm install) || exit 1
 echo "build studio"
-(cd studio && npm run build)       || exit 1
+(cd studio && npm run build) || exit 1
+npm install || exit 1
 echo "done"
